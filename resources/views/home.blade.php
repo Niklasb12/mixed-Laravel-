e1d5e5278a7bd327f47b13fe1c806bac2c878a1b
Caroline Olby <carolineolby@Carolines-MacBook-Pro.local>
2020-06-04 08:29:23 +0200

    changed background in login and signup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,4 @@
 
 @section('content') 
     <h1 class="Home_Headline">  M | X E D </h1> 
-    <h1 class="Home_Headline">  M | X E D </h1> 
-    <h1 class="Home_Headline">  M | X E D </h1> 
 @endsection
