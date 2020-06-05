4b7baf5b95fe1012c28463c8e8ffda70c0b650aa
Niklas Borg <niklasborg12@gmail.com>
2020-06-05 13:06:57 +0200

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layout') 
 
 @section('content') 
+    <h1 class="Home_Headline">  M | X E D </h1>
     <h1 class="Home_Headline">  M | X E D </h1> 
+    <h1 class="Home_Headline">  M | X E D </h1> 
+    <h1 class="Home_Headline">  M | X E D </h1> 
+    <h1 class="Home_Headline">  M | X E D </h1> 
+    <h1 class="Home_Headline">  M | X E D </h1>  
 @endsection
